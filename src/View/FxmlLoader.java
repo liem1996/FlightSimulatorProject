210f40e6c4eb938f642b0b31e6b206bae534d90d
amit ben basat <amitbasat124@gmail.com>
2021-05-05 20:19:55 +0300

    adding the fxml loder for each part of the gui and making it one gui

--- src/View/FxmlLoader.java
+++ src/View/FxmlLoader.java
@@ -1,33 +1,60 @@
 
-/*
+
 package View;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
 import java.net.URL;
+
 
 public class FxmlLoader {
 
-    private Pane view;
 
-    public Pane getPage(String fileName){
-        try{
-            URL fileUrl = Main.class.getResource("/View/" + fileName + ".fxml");
-            if (fileUrl == null){
-                throw  new java.io.FileNotFoundException("Fxml can't be found");
-
-            }
-
-            view = new FXMLLoader().load(fileUrl);
+    /*
+    public AnchorPane createAnchor(String Filename){
+        AnchorPane root = new AnchorPane();
+        try {
+           root = FXMLLoader.load(getClass().getResource(Filename));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (Exception e)
-        {
-            System.out.println("No page " + fileName + " please check FxmlLoader.");
-        }
-        return view;
+        return root;
+
     }
+
+     */
+//אם יש לך רעיון לפונקציה שתוכל לקחת כל פעם שם אחר תגידי לי
+
+    public void start(Stage primaryStage) {
+       try {
+
+           AnchorPane root = FXMLLoader.load(getClass().getResource("MainWindowController.fxml"));
+           AnchorPane Joystick= FXMLLoader.load(getClass().getResource("JoyStick.fxml"));
+           AnchorPane Clocks = FXMLLoader.load(getClass().getResource("Clocks.fxml"));
+           AnchorPane Player = FXMLLoader.load(getClass().getResource("Player.fxml"));
+           AnchorPane ChartsList = FXMLLoader.load(getClass().getResource("ChartsList.fxml"));
+            root.getChildren().setAll(Joystick,Clocks,Player,ChartsList);
+            Scene s = new Scene(root);
+            primaryStage.setScene(s);
+
+        } catch (IOException e) {
+            System.err.println("Error loading EventHandlerDemo.fxml!");
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
 
- */
+
+
+
--- src/View/Main.java
+++ src/View/Main.java
@@ -4,16 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../ModelView/sample.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindowController.fxml"));
         primaryStage.setTitle("Flight Simulator");
-        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.setScene(new Scene(root, 1700, 1200));
+        FxmlLoader fx =new FxmlLoader();
+        fx.start(primaryStage);
         primaryStage.show();
     }
 
