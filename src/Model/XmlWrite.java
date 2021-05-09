3907a3b59e422e7ef1b398503b09ef53cab495c8
amit ben basat <amitbasat124@gmail.com>
2021-05-09 18:11:40 +0300

    adding the model and the viewmodel and starting to do the controller of the player and binding it with a function to the viemodel and the model

---
+++ src/Model/XmlWrite.java
@@ -0,0 +1,51 @@
+package Model;
+
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class XmlWrite {
+
+    public void serializeToXML(property col){
+        //write a object in java into an xml, write the min,max and name of the column
+        try {
+            FileOutputStream xml = new FileOutputStream("Properties.xml");
+            XMLEncoder encoder = new XMLEncoder(xml);
+            encoder.writeObject(col);
+            encoder.close();
+            xml.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public property deserializeFromXML(){
+        property col = new property();
+        //write an xml into an java object, write the port'ip and index of the name of the column
+        try {
+            FileInputStream obj = new FileInputStream("Properties.xml");
+            XMLDecoder dec = new XMLDecoder(obj);
+            col = (property) dec.readObject();
+            dec.close();
+            obj.close();
+            return col;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return col;
+    }
+
+
+
+}
---
+++ src/Model/property.java
@@ -0,0 +1,72 @@
+package Model;
+
+import java.util.HashMap;
+
+import java.util.HashMap;
+
+public class property {
+
+        public HashMap<String,Integer> nameColIndex;
+        public int port;
+        public int ip;
+        public double timeperSeconed;
+        public HashMap<String,Integer> minimum;
+        public HashMap<String,Integer> maximum;
+
+    public property() {
+        this.nameColIndex = new HashMap<>();
+        this.port =50;
+        this.ip = 10;
+        this.timeperSeconed=1;
+        this.minimum=new HashMap<>();
+        this.maximum=new HashMap<>();
+    }
+
+    public HashMap<String, Integer> getNameColIndex() {
+        return nameColIndex;
+    }
+
+    public void setNameColIndex(HashMap<String, Integer> nameColIndex) {
+        this.nameColIndex = nameColIndex;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getIp() {
+        return ip;
+    }
+
+    public void setIp(int ip) {
+        this.ip = ip;
+    }
+
+    public double getTimeperSeconed() {
+        return timeperSeconed;
+    }
+
+    public void setTimeperSeconed(double timeperSeconed) {
+        this.timeperSeconed = timeperSeconed;
+    }
+
+    public HashMap<String, Integer> getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(HashMap<String, Integer> minimum) {
+        this.minimum = minimum;
+    }
+
+    public HashMap<String, Integer> getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(HashMap<String, Integer> maximum) {
+        this.maximum = maximum;
+    }
+}
