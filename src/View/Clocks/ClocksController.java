a35a68eca54cf0727246475f3504e70e8cad73c6
amit ben basat <amitbasat124@gmail.com>
2021-06-13 15:44:38 +0300

    adding the new viewmodel and model with the new classes of the intalize

--- src/View/Clocks/ClocksController.java
+++ src/View/Clocks/ClocksController.java
@@ -1,6 +1,35 @@
 package View.Clocks;
 
+import View.fxmlController.MainWindowController;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ClocksController {
+    @FXML
+    public Label altimeter;
+
+
+
+    public String getAltimeter() {
+        return altimeter.getText();
+    }
+
+    public ClocksController() {
+        this.altimeter = new Label("MY LABEL");
+    }
+
+
+
 
 
 }
